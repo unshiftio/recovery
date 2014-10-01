761c62de93be94902e121287b57ce940f4839acf
Arnout Kazemier <info@3rd-Eden.com>
2014-10-01 11:40:59 +0200

    [init] Initial working, tested, 100% coverage version

---
+++ index.js
@@ -0,0 +1,204 @@
+'use strict';
+
+var Tick = require('tick-tock')
+  , one = require('one-time');
+
+/**
+ * Attempt to recover your connection with reconnection attempt.
+ *
+ * @constructor
+ * @param {EventEmitter} eventemitter EventEmitter where we emit our events on.
+ * @param {Object} options Configuration
+ * @api public
+ */
+function Recovery(eventemitter, options) {
+  if (!(this instanceof Recovery)) return new Recovery(eventemitter, options);
+  options = options || {};
+
+  this.fn = null;       // Stores the callback.
+  this.timers = {};     // It was either manually setting this or a Tick.call(this);
+  this.attempt = null;  // Stores the current reconnect attempt.
+  this.events = eventemitter;
+
+  this.max = this.default('max', options);
+  this.min = this.default('min', options);
+  this.factor = this.default('factor', options);
+  this.retries = this.default('retries', options);
+  this.timeout = this.default('timeout', options);
+}
+
+Recovery.prototype = new Tick();
+Recovery.prototype.constructor = Recovery;
+
+Recovery.max = Infinity;          // Maximum delay.
+Recovery.min = '500 ms';          // Minimum delay.
+Recovery.retries = 10;            // Maximum amount of retries.
+Recovery.factor = 2;              // Exponential back off factor.
+Recovery.timeout = '30 seconds';  // Maximum timeout for the request to answer.
+
+/**
+ * Returns sane defaults about a given value.
+ *
+ * @param {String} name Name of property we want>
+ * @param {Object} opts User supplied options we want to check.
+ * @returns {Number} Some default value.
+ * @api private
+ */
+Recovery.prototype.default = function defaults(name, opts) {
+  return Tick.parse(name in opts ? opts[name] : (name in this ? this[name] : Recovery[name]));
+};
+
+/**
+ * Start a new reconnect procedure.
+ *
+ * @returns {Recovery}
+ * @api public
+ */
+Recovery.prototype.reconnect = function reconnect() {
+  var recovery = this;
+
+  return this.backoff(function backedoff(err, opts) {
+    opts.duration = (+new Date()) - opts.start;
+
+    if (err) return recovery.events.emit('reconnect failed', err, opts);
+    recovery.events.emit('reconnected', opts);
+  }, this.attempt);
+};
+
+/**
+ * Reconnecting failed. Start over again.
+ *
+ * @param {Error} err Optional error message.
+ * @returns {Boolean} Successfully notified of failure.
+ * @api public
+ */
+Recovery.prototype.failed = function failed(err) {
+  if ('function' === typeof this.fn) {
+    this.fn(err || new Error('Failed to reconnect'));
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Successful reconnect. Clean up things.
+ *
+ * @returns {Boolean} Successfully notified of success.
+ * @api public
+ */
+Recovery.prototype.success = function success() {
+  if ('function' === typeof this.fn) {
+    this.fn();
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Exponential back off algorithm for retry operations. It uses an randomized
+ * retry so we don't DDOS our server when it goes down under pressure.
+ *
+ * @param {Function} fn Callback to be called after the timeout.
+ * @param {Object} opts Options for configuring the timeout.
+ * @returns {Recovery}
+ * @api private
+ */
+Recovery.prototype.backoff = function backoff(fn, opts) {
+  var recovery = this;
+
+  opts = opts || recovery.attempt || {};
+
+  //
+  // Bailout when we already have a back off process running. We shouldn't call
+  // the callback then.
+  //
+  if (opts.backoff) return recovery;
+
+  opts.max = recovery.default('max', opts);
+  opts.min = recovery.default('min', opts);
+  opts.factor = recovery.default('factor', opts);
+  opts.retries = recovery.default('retries', opts);
+  opts.timeout = recovery.default('timeout', opts);
+
+  opts.start = +opts.start || +new Date();
+  opts.duration = +opts.duration || 0;
+  opts.attempt = +opts.attempt || 0;
+
+  //
+  // Bailout if we are about to make to much attempts. Please note that we use
+  // `>` because we already incremented the value above.
+  //
+  if (opts.attempt === opts.retries) {
+    recovery.attempt = recovery.fn = null;
+    fn.call(recovery, new Error('Unable to recover'), opts);
+    return recovery;
+  }
+
+  //
+  // Prevent duplicate back off attempts using the same options object and
+  // increment our attempt as we're about to have another go at this thing.
+  //
+  opts.backoff = true;
+  opts.attempt++;
+
+  recovery.attempt = opts;
+
+  //
+  // Calculate the timeout, but make it randomly so we don't retry connections
+  // at the same interval and defeat the purpose. This exponential back off is
+  // based on the work of:
+  //
+  // http://dthain.blogspot.nl/2009/02/exponential-backoff-in-distributed.html
+  //
+  opts.scheduled = opts.attempt !== 1
+    ? Math.min(Math.round(
+        (Math.random() + 1) * opts.min * Math.pow(opts.factor, opts.attempt)
+      ), opts.max)
+    : opts.min;
+
+  recovery.setTimeout('reconnect', function delay() {
+    opts.duration = (+new Date()) - opts.start;
+    recovery.clear('reconnect');
+    opts.backoff = false;
+
+    //
+    // Create a `one` function which can only be called once. So we can use the
+    // same function for different types of invocations to create a much better
+    // and usable API.
+    //
+    var connect = recovery.fn =  one(function connect(err) {
+      recovery.clear('reconnect', 'timeout');
+      recovery.fn = null;
+
+      if (err) return recovery.backoff(fn, opts);
+
+      recovery.attempt = null;
+      fn.call(recovery, undefined, opts);
+    });
+
+    recovery.setTimeout('timeout', function timeout() {
+      var err = new Error('Failed to reconnect in a timely manner');
+      opts.duration = (+new Date()) - opts.start;
+
+      recovery.events.emit('reconnect timeout', err, opts);
+      connect(err);
+    }, opts.timeout);
+
+    recovery.events.emit('reconnect', connect, opts);
+  }, opts.scheduled);
+
+  //
+  // Emit a `reconnecting` event with current reconnect options. This allows
+  // them to update the UI and provide their users with feedback.
+  //
+  recovery.events.emit('reconnecting', opts);
+
+  return recovery;
+};
+
+//
+// Expose the module.
+//
+module.exports = Recovery;
---
+++ test.js
@@ -0,0 +1,273 @@
+describe('recovery', function () {
+  'use strict';
+
+  var EventEmitter = require('events').EventEmitter
+    , Tick = require('tick-tock')
+    , assume = require('assume')
+    , Recovery = require('./')
+    , recovery
+    , emitter;
+
+  beforeEach(function () {
+    emitter = new EventEmitter();
+    recovery = new Recovery(emitter);
+  });
+
+  afterEach(function () {
+    recovery.clear();
+  });
+
+  this.timeout(60000);
+
+  it('is exported as a function', function () {
+    assume(Recovery).is.a('function');
+  });
+
+  it('returns a new instance if constructed without new', function () {
+    recovery = Recovery(emitter);
+    assume(recovery).is.instanceOf(Recovery);
+  });
+
+  describe('#default', function () {
+    var opts = {
+      min: 1000
+    };
+
+    it('finds the property in the supplied object', function () {
+      assume(recovery.default('min', opts)).to.equal(opts.min);
+      assume(recovery.default('min', {})).to.not.equal(opts.min);
+    });
+
+    it('defaults to the value specified on the constructor', function () {
+      recovery.min = 4241;
+
+      assume(recovery.default('min', {})).to.equal(4241);
+    });
+
+    it('automatically transforms the value to a number', function () {
+      delete recovery.min;
+
+      assume(Recovery.min).to.be.a('string');
+      assume(recovery.default('min', {})).to.a('number');
+    });
+
+    it('defaults to the "global" Recovery instance', function () {
+      delete recovery.min;
+
+      assume(recovery.default('min', {})).to.equal(Tick.parse(Recovery.min));
+    });
+
+    it('integrates in the constructor', function () {
+      assume(recovery.min).to.equal(Tick.parse(Recovery.min));
+      assume(Recovery.min).to.not.equal(10);
+
+      Recovery.min = 10;
+      recovery = new Recovery();
+      assume(recovery.min).to.equal(Tick.parse(Recovery.min));
+      assume(Recovery.min).to.equal(10);
+    });
+  });
+
+  describe('#reconnect', function () {
+    it('emits `reconnecting` when starting', function (next) {
+      emitter.once('reconnecting', function (opts) {
+        assume(opts).is.a('object');
+        assume(opts.attempt).to.equal(1);
+        assume(opts.retries).to.equal(recovery.retries);
+        assume(opts.max).to.equal(recovery.max);
+        assume(opts.mix).to.equal(recovery.mix);
+        assume(opts.scheduled).is.least(opts.min);
+        assume(opts.scheduled).is.most(opts.max);
+        assume(opts.duration).is.equal(0);
+
+        next();
+      });
+
+      recovery.reconnect();
+    });
+
+    it('emits `reconnect` when you need to start the reconnect', function (next) {
+      emitter.once('reconnect', function (fn, opts) {
+        assume(fn).is.a('function');
+
+        assume(opts).is.a('object');
+        assume(opts.attempt).to.equal(1);
+        assume(opts.retries).to.equal(recovery.retries);
+        assume(opts.max).to.equal(recovery.max);
+        assume(opts.mix).to.equal(recovery.mix);
+        assume(opts.scheduled).is.least(opts.min);
+        assume(opts.scheduled).is.most(opts.max);
+
+        next();
+      });
+
+      recovery.reconnect();
+    });
+
+    it('emits `reconnect timeout` when the reconnect attempt timed out', function (next) {
+      emitter.once('reconnect timeout', function (err, opts) {
+        assume(err).is.a('error');
+        assume(err.message).contains('time');
+
+        assume(opts).is.a('object');
+        assume(opts.attempt).to.equal(1);
+        assume(opts.retries).to.equal(recovery.retries);
+        assume(opts.max).to.equal(recovery.max);
+        assume(opts.mix).to.equal(recovery.mix);
+        assume(opts.scheduled).is.least(opts.min);
+        assume(opts.scheduled).is.most(opts.max);
+
+        next();
+      });
+
+      recovery.timeout = 100;
+      recovery.reconnect();
+    });
+
+    it('emits `reconnect failed` when all attempts failed', function (next) {
+      var attempts = 0
+        , start = Date.now();
+
+      emitter.on('reconnecting', function (opts) {
+        attempts++;
+
+        assume(opts.attempt).to.equal(attempts);
+        assume(opts.retries).to.equal(recovery.retries);
+        assume(opts.max).to.equal(recovery.max);
+        assume(opts.mix).to.equal(recovery.mix);
+        assume(opts.scheduled).is.least(opts.min);
+        assume(opts.scheduled).is.most(opts.max);
+
+        if (attempts === 1) return;
+
+        assume(opts.duration).to.most(Date.now() - start);
+        assume(opts.duration).to.least((Date.now() - start) - 10);
+      });
+
+      emitter.on('reconnect timeout', function (err, opts) {
+        assume(opts.attempt).to.equal(attempts);
+      });
+
+      emitter.on('reconnect failed', function (err, opts) {
+        assume(err).is.a('error');
+        assume(err.message).contains('recover');
+
+        assume(opts.attempt).to.equal(recovery.retries);
+        assume(opts.attempt).to.equal(opts.retries);
+        assume(opts.duration).to.most(Date.now() - start);
+        assume(opts.duration).to.least((Date.now() - start) - 10);
+
+        assume(recovery.fn).to.equal(null);
+        assume(recovery.failed()).is.false();
+        assume(recovery.success()).is.false();
+
+        next();
+      });
+
+      recovery.timeout = 100;
+      recovery.max = 2000;
+      recovery.reconnect();
+    });
+
+    it('emits a `reconnected` event for a successful connection', function (next) {
+      var start = Date.now();
+
+      emitter.on('reconnected', function (opts) {
+        assume(opts.attempt).equals(2);
+        assume(opts.duration).to.most(Date.now() - start);
+        assume(opts.duration).to.least((Date.now() - start) - 10);
+
+        assume(recovery.fn).to.equal(null);
+        assume(recovery.failed()).is.false();
+        assume(recovery.success()).is.false();
+
+        next();
+      });
+
+      emitter.on('reconnect', function (fn, opts) {
+        if (opts.attempt === 1) return setTimeout(function () {
+          fn(new Error('Nope, we failed'));
+        }, 50);
+
+        setTimeout(fn, 50);
+      });
+
+      recovery.timeout = 100;
+      recovery.reconnect();
+    });
+
+    it('can use the .success and .failed APIs', function (next) {
+      var start = Date.now();
+
+      emitter.on('reconnected', function (opts) {
+        assume(opts.attempt).equals(2);
+        assume(opts.duration).to.most(Date.now() - start);
+        assume(opts.duration).to.least((Date.now() - start) - 10);
+
+        assume(recovery.fn).to.equal(null);
+        assume(recovery.failed()).is.false();
+        assume(recovery.success()).is.false();
+
+        next();
+      });
+
+      emitter.on('reconnect', function (fn, opts) {
+        if (opts.attempt === 1) return setTimeout(function () {
+          assume(recovery.failed()).is.true();
+        }, 50);
+
+        setTimeout(function () {
+          assume(recovery.success()).is.true();
+        }, 50);
+      });
+
+      recovery.timeout = 100;
+      recovery.reconnect();
+    });
+
+    it('doesnt allow another reconnection attempt while busy', function (next) {
+      var attempts = 0;
+
+      emitter.on('reconnect', function () {
+        recovery.success();
+      });
+
+      emitter.on('reconnected', function () {
+        next();
+      });
+
+      emitter.on('reconnecting', function (fn, opts) {
+        attempts++;
+
+        if (attempts > 1) throw new Error('I should only reconnect once');
+      });
+
+      recovery.timeout = 100;
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+      recovery.reconnect();
+    });
+  });
+});
